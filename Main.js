c5a1cbbd817d68432ab8eb256c04ac38c7b401ff
Carlos Coronel <62563903+CarlosCoronel24@users.noreply.github.com>
2022-03-19 00:58:43 -0700

    Commit Inicio Prpoyecto

---
+++ Main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    // jQuery methods go here...
+
+    
+        $("#btnEjemplo_1").click(function(){
+          $("p").hide();
+        });
+
+        $("#btnEjemplo_2").click(function(){
+          $(".test").hide();
+        });
+
+        $("#btnEjemplo_3").click(function(){
+          $(".test").hide();
+        });
+      
+        
+  });
